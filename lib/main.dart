e5a6cb1cb8b07802ff861c76899b29b727a68d40
Biplob Dash <110329225+BiplobDash@users.noreply.github.com>
2024-04-10 09:52:54 +0600

    Project Setup

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "${AppStrings.appName} Web",
       home: const  HomeScreen(),
     );
