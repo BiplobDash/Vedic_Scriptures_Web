39da15e92d231060b986af1d78d900866bfd3475
Biplob Dash <110329225+BiplobDash@users.noreply.github.com>
2024-04-10 09:51:52 +0600

    first commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const appBarColor = Colors.purple;
+  static const bgColor = Color(0xffffca86);
+  static const whiteColor = Colors.white;
+}
---
+++ lib/utils/strings.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  static String appName = 'Vedic Scriptures';
+  static String powerVeda = 'Power By Veda';
+  static String developedBy = 'Developed By Biplob Dash';
+  static String pdfAppbarName = 'অমৃতের জ্ঞান';
+}
